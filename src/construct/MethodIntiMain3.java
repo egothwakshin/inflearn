968aa89f40a0ed97d0ec9ee7c251166903299ade
egothwakshin <feelso0226@naver.com>
2024-10-24 17:28:21 +0900

    2024-10-24 17:28

--- src/construct/MethodIntiMain3.java
+++ src/construct/MethodIntiMain3.java
@@ -12,7 +12,7 @@ public class MethodIntiMain3 {
         MemberInit[] members = {member1, member2};
 
         for (MemberInit s : members) {
-            System.out.println("아름: " + s.name + ", 나이: " + s.age + ", 성적: " + s.grade);
+            System.out.println("이름: " + s.name + ", 나이: " + s.age + ", 성적: " + s.grade);
         }
     }
 }
