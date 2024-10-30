3cc3130999e353876e6d08a3975056551396ecfd
egothwakshin <feelso0226@naver.com>
2024-10-30 17:44:05 +0900

    2024-10-30 17:43

--- src/access/SpeakerMain.java
+++ src/access/SpeakerMain.java
@@ -4,7 +4,7 @@ public class SpeakerMain {
 
     public static void main(String[] args) {
         Speaker speaker = new Speaker(90);
-        speaker.showVolume();
+        speaker.showVolume();   //현재 음량: 90
 
         speaker.volumeUp();
         speaker.showVolume();
@@ -12,8 +12,9 @@ public class SpeakerMain {
         speaker.volumeUp();
         speaker.showVolume();
 
+        //필드에 직접 접근
         System.out.println("volume 필드 직접 접근 수정");
-        //speaker .volume = 200;
+        //speaker.volume = 200;
         speaker.showVolume();
     }
 }
--- src/access/b/PublicClassOuterMain.java
+++ src/access/b/PublicClassOuterMain.java
@@ -3,7 +3,7 @@ package access.b;
 
 import access.a.PublicClass;
 
-public class PublicClassOuterMain {
+public class      PublicClassOuterMain {
     public static void main(String[] args) {
         PublicClass publicClass = new PublicClass();
 
